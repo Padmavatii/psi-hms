372218ec399683be40c3c1f42863b5a394574904
Padmavati Hosagoudar <phosagoudar@primesoft.net>
2024-05-24 12:20:27 +0530

    first commit

---
+++ src/main/java/psi_hms/PsiHmsApplication.java
@@ -0,0 +1,13 @@
+package psi_hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiHmsApplication.class, args);
+	}
+
+}
